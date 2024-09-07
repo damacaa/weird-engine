f06189e0a01b4bb8bbbb33ef7d1366b2ee2d4a7b
Daniel Martí <49535803+damacaa@users.noreply.github.com>
2024-09-07 12:06:38 +0200

    Function to translate screen coordinates to world coordinates in 2d

--- src/weird-engine/ecs/Components/Camera.h
+++ src/weird-engine/ecs/Components/Camera.h
@@ -6,7 +6,6 @@ namespace ECS
 {
 	struct Camera : public Component 
 	{
-
 		WeirdRenderer::Camera camera;
 
 		Camera() : camera(vec3(0.0f))
@@ -19,6 +18,17 @@ namespace ECS
 
 		}
 
+		static glm::vec2 screenPositionToWorldPosition2D(Transform cameraTransform, vec2 screenPosition)
+		{
+			vec2 resolution(1200, 800);
+			vec2 halfResolution(600, 400);
+
+			vec2 screenPositionCorrected(screenPosition.x, resolution.y - screenPosition.y);
+			vec2 uv = (2.0f * screenPositionCorrected - resolution) / resolution.y;
+			vec2 position = (vec2)cameraTransform.position + ((screenPositionCorrected - halfResolution) * (cameraTransform.position.z / halfResolution.y));
+
+			return position;
+		}
 
 	};
 }

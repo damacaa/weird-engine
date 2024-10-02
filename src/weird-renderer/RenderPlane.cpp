b59cc58af7bd1afcd01120faa8502b32d73b7a4f
Daniel Martí <49535803+damacaa@users.noreply.github.com>
2024-10-02 18:58:52 +0200

    [WIP] Default distance texture set to max distance

--- src/weird-renderer/RenderPlane.cpp
+++ src/weird-renderer/RenderPlane.cpp
@@ -82,12 +82,22 @@ namespace WeirdRenderer
 		glGenTextures(1, &m_colorTexture);
 		glBindTexture(GL_TEXTURE_2D, m_colorTexture);
 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+		unsigned char* textureData = new unsigned char[width * height * 4];
+
+		for (size_t i = 0; i < width * height * 4; i++)
+		{
+			textureData[i] = 255;
+		}
+
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // Prevents edge bleeding
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Prevents edge bleeding
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorTexture, 0);
+
+		delete[] textureData;
 
 		// Create a depth texture
 		glGenTextures(1, &m_depthTexture);

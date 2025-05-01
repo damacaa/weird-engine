de39a94c83278ef10e6b4dbc9db983d017f4fc0d
Daniel Martí <49535803+damacaa@users.noreply.github.com>
2025-05-01 17:44:54 +0200

    Update to OpenGL 4.6

--- src/weird-renderer/Renderer.cpp
+++ src/weird-renderer/Renderer.cpp
@@ -31,8 +31,8 @@ namespace WeirdEngine
 
 			// Tell GLFW what version of OpenGL we are using
 			// In this case we are using OpenGL 3.3
-			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 			// Tell GLFW we are using the CORE profile
 			// So that means we only have the modern functions
 			glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);

7e2aaf23cbd5b9ecd28fc34d4a8f9df618a09962
Daniel Martí <49535803+damacaa@users.noreply.github.com>
2025-05-20 20:52:11 +0200

    More camera control

--- include/weird-renderer/Camera.h
+++ include/weird-renderer/Camera.h
@@ -34,6 +34,7 @@ namespace WeirdEngine
 			glm::mat4 view;
 
 			float fov = 45.0f;
+			bool lookAtMode = true;
 
 			// Updates the camera matrix to the Vertex Shader
 			void updateMatrix(float nearPlane, float farPlane, int width, int height);
--- src/weird-renderer/Camera.cpp
+++ src/weird-renderer/Camera.cpp
@@ -11,10 +11,25 @@ namespace WeirdEngine
 			position = position;
 		}
 
+		const auto RIGHT = glm::vec3(1.0f, 0.0f, 0.0f);
+		const auto UP = glm::vec3(0.0f, 1.0f, 0.0f);
+		const auto FORWARD = glm::vec3(0.0f, 0.0f, 1.0f);
+
 		void Camera::updateMatrix(float nearPlane, float farPlane, int width, int height)
 		{
-			// Makes camera look in the right direction from the right position
-			view = glm::lookAt(position, position + orientation, up);
+			if (lookAtMode)
+			{
+				// Makes camera look in the right direction from the right position
+				view = glm::lookAt(position, position + orientation, up);
+			}
+			else
+			{
+				view = glm::mat4(1.0f);
+
+				view = glm::rotate(view, orientation.x, RIGHT);
+				view = glm::rotate(view, orientation.y, UP);
+				view = glm::rotate(view, orientation.z, FORWARD);
+			}
 
 			// Adds perspective to the Scene
 			projection = glm::perspective(glm::radians(fov), (float)width / height, nearPlane, farPlane);

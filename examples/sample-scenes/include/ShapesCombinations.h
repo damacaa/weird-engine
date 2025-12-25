cf886a7bcb983bb10cef17ca011c5e70e18a972f
Daniel Martí <49535803+damacaa@users.noreply.github.com>
2025-12-25 12:01:19 +0100

    Update ShapesCombinations.h

--- examples/sample-scenes/include/ShapesCombinations.h
+++ examples/sample-scenes/include/ShapesCombinations.h
@@ -121,7 +121,8 @@ private:
 
     {
       auto& text = m_ecs.getComponent<UITextRenderer>(m_text);
-      text.text = "Balls:" + std::to_string(static_cast<int>(m_ecs.getComponentArray<SDFRenderer>()->getSize()));
+      bool hideText = Input::GetKey(Input::LeftAlt);
+      text.text = hideText ? "" : "Balls:" + std::to_string(static_cast<int>(m_ecs.getComponentArray<SDFRenderer>()->getSize()));
       text.dirty = true;
 
       auto& textTransform = m_ecs.getComponent<Transform>(m_text);

22b2415573114c8ccae523974e4c782e27b83acd
Daniel Martí <49535803+damacaa@users.noreply.github.com>
2026-03-13 18:40:18 +0100

    Component array optimization

--- include/weird-engine/ecs/ComponentArray.h
+++ include/weird-engine/ecs/ComponentArray.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <unordered_map>
 #include <stdexcept>
 
@@ -9,88 +10,84 @@
 namespace WeirdEngine
 {
 
-		// ComponentArray to store components of a specific type
-		template <typename T>
-		class ComponentArray
-		{
-		public:
-			std::array<T, MAX_ENTITIES> values;
-			size_t size = 0;
+    // ComponentArray to store components of a specific type
+    template <typename T>
+    class ComponentArray
+    {
+    public:
+        std::array<T, MAX_ENTITIES> values;
+        size_t size = 0;
 
-			void insertData(Entity entity, T component)
-			{
-				entityToIndexMap[entity] = size; // ERROR: this is null
-				indexToEntityMap[size] = entity;
-				values[size] = component;
-				++size;
-			}
+        void insertData(Entity entity, T component)
+        {
+            entityToIndexMap[entity] = size;
+            indexToEntityMap[size] = entity;
+            values[size] = component;
+            ++size;
+        }
 
-			T& getNewComponent(Entity entity)
-			{
-				entityToIndexMap[entity] = size;
-				indexToEntityMap[size] = entity;
+        T& getNewComponent(Entity entity)
+        {
+            entityToIndexMap[entity] = size;
+            indexToEntityMap[size] = entity;
 
-				return values[size++];
-			}
+            return values[size++];
+        }
 
-			void removeData(Entity entity)
-			{
-				size_t indexOfRemovedEntity = entityToIndexMap[entity];
-				size_t indexOfLastElement = size - 1;
-				values[indexOfRemovedEntity] = values[indexOfLastElement];
-				values[indexOfLastElement] = T(); // This might be a mistake...
+        void removeData(Entity entity)
+        {
+            size_t indexOfRemovedEntity = entityToIndexMap[entity];
+            size_t indexOfLastElement = size - 1;
+            values[indexOfRemovedEntity] = values[indexOfLastElement];
 
-				Entity entityOfLastElement = indexToEntityMap[indexOfLastElement];
-				entityToIndexMap[entityOfLastElement] = indexOfRemovedEntity;
-				indexToEntityMap[indexOfRemovedEntity] = entityOfLastElement;
+            Entity entityOfLastElement = indexToEntityMap[indexOfLastElement];
+            entityToIndexMap[entityOfLastElement] = indexOfRemovedEntity;
+            indexToEntityMap[indexOfRemovedEntity] = entityOfLastElement;
 
-				entityToIndexMap.erase(entity);
-				indexToEntityMap.erase(indexOfLastElement);
+            --size;
+        }
 
-				--size;
-			}
+        T& getDataFromEntity(Entity entity)
+        {
+            return values[entityToIndexMap[entity]];
+        }
 
-			T& getDataFromEntity(Entity entity)
-			{
-				return values[entityToIndexMap[entity]];
-			}
+        T& getDataAtIdx(size_t idx)
+        {
+            return values[idx];
+        }
 
-			T& getDataAtIdx(size_t idx)
-			{
-				return values[idx];
-			}
+        T& getLastData()
+        {
+            return values[size - 1];
+        }
 
-			T& getLastData()
-			{
-				return values[size - 1];
-			}
+        bool hasData(Entity entity)
+        {
+            return entityToIndexMap[entity] < size;
+        }
 
-			bool hasData(Entity entity)
-			{
-				return entityToIndexMap.size() > 0 && entityToIndexMap.find(entity) != entityToIndexMap.end();
-			}
+        // Overload [] operator for non-const objects (modifiable)
+        T& operator[](unsigned int index)
+        {
 
-			// Overload [] operator for non-const objects (modifiable)
-			T& operator[](unsigned int index)
-			{
+            if (index < 0 || index >= size)
+            {
+                throw std::out_of_range("Index out of range");
+            }
 
-				if (index < 0 || index >= size)
-				{
-					throw std::out_of_range("Index out of range");
-				}
+            return values[index];
+        }
 
-				return values[index];
-			}
+        // Function to get the size of the array
+        int getSize() const
+        {
+            return size;
+        }
 
-			// Function to get the size of the array
-			int getSize() const
-			{
-				return size;
-			}
+    private:
+        std::array<size_t, MAX_ENTITIES> entityToIndexMap = {};
+        std::array<Entity, MAX_ENTITIES> indexToEntityMap = {};
+    };
 
-		private:
-			std::unordered_map<Entity, size_t> entityToIndexMap;
-			std::unordered_map<size_t, Entity> indexToEntityMap;
-		};
-	
 }
